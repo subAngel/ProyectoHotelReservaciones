7d1e7de6e5337dc680372119d8fec17e9d8e7572
subAngel <whoangel.agl@gmail.com>
2021-07-13 21:10:48 -0500

    reservaciones de hotel

---
+++ src/SQL/ConexionBD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author angel
+ */
+public class ConexionBD {
+
+    private static String url = "jdbc:mysql://localhost/reservacion_habitaciones";
+    private static String usuario = "root";
+    private static String password = "root";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+            JOptionPane.showMessageDialog(null, "Conexion exitosa");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conexion" + e);
+        }
+
+        return conexion;
+    }
+
+
+}
